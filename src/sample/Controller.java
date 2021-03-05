ab313b22e4c32f9241e0748c3e78580f32a813a4
Kevin Wood <kevinlw14@gmail.com>
2021-03-05 02:08:42 -0500

    Rough build with working algorithm.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+
+public class Controller implements ConverterViewDelegate {
+
+    private Converter converter;
+    private ConverterView view;
+
+    public Controller(ConverterView view) {
+        this.view = view;
+        this.view.setDelegate(this);
+        this.converter = new Converter();
+    }
+
+    @Override
+    public void convertButtonPressed(ActionEvent event) {
+        String expression = view.getInfixTextField().getText();
+        converter.convert(expression);
+    }
+
+    @Override
+    public void showButtonPressed(ActionEvent event) {
+        String postfix = converter.postfixExpression();
+        view.getPostfixLabel().setText(postfix);
+    }
 }
